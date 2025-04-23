0b8381e1d2260be0206ed49a382bee876b15b96e
LorenzoLatinnePXL <11701160@student.pxl.be>
2025-04-23 10:43:56 +0200

    Start of labo Inheritance

---
+++ Labo-Inheritance/ClassLib/Log.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ClassLib
+{
+    public class Log
+    {
+        private Dictionary<DateTime, string> _log = new Dictionary<DateTime, string>();
+        private string _fileName;
+
+        public Log(string fileName)
+        {
+            _fileName = fileName;
+        }
+
+        public Log(SqlConnection conn)
+        {
+            _fileName = conn.ConnectionString;
+        }
+
+        public virtual void Write(string message)
+        {
+            using (FileStream fs = new FileStream(_fileName, FileMode.Append, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine($"{DateTime.Now} - {message}");
+                }
+            }
+            _log.Add(DateTime.Now, message);
+        }
+
+        public void DisplayLog()
+        {
+            foreach (KeyValuePair<DateTime, string> kvp in _log)
+            {
+                Console.WriteLine($"{kvp.Key} : {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ Labo-Inheritance/Labo-Inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using ClassLib;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Log log = new Log("file1.txt");
+        log.Write("This is a normal log message.");
+        Console.WriteLine("Writing the normal log:");
+        log.DisplayLog();
+        Console.WriteLine();
+
+        ActivityLog activityLog = new ActivityLog("file2.txt");
+        activityLog.Write("Person X entered the building.");
+        activityLog.Write("Person Y entered the building.");
+        activityLog.Write("Person X left the building.");
+        activityLog.Write("Person Z entered the building.");
+        Console.WriteLine("Writing the activity log:");
+        activityLog.DisplayLog();
+        Console.WriteLine();
+
+        ErrorLog errorLog = new ErrorLog("file3.txt");
+        errorLog.Write("This is an error log message.");
+        Console.WriteLine("Writing the error log:");
+        errorLog.DisplayLog();
+        Console.WriteLine();
+
+
+        Console.ReadLine();
+    }
+}
